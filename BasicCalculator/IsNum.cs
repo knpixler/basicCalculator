1be3b7b3954a6fd8721b53cfb1bea0fd65e1a496
Kevin Pixler <53046616+knpixler@users.noreply.github.com>
2019-10-07 15:03:21 -0400

    Add files via upload

---
+++ BasicCalculator/IsNum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BasicCalculator
+{
+    class IsNum
+    {
+        decimal input;
+        public decimal Anum()
+        {
+            bool checkIt = true;
+            decimal returned;
+
+
+            while (checkIt)
+            {
+                var incoming = Console.ReadLine();
+
+                if (!decimal.TryParse(incoming, out returned))
+                {
+                    Console.WriteLine("That is not a number.");
+                    Console.WriteLine("Enter a number.");
+                }
+                else if (decimal.TryParse(incoming, out returned))
+                {
+                    input = returned;
+                    checkIt = false;
+                }
+
+            }
+            return input;
+
+        }
+    }
+}
